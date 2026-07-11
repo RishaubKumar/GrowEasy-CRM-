78c13bc77318eea4e5f7f328bcd17b1c50b0e006
Rishaub Kumar <rishaubkumar534@gmail.com>
2026-07-11 16:40:02 +0530

    adding vercel file

--- server/index.js
+++ server/index.js
@@ -11,4 +11,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use("/api/csv", csvRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+module.exports = app;
